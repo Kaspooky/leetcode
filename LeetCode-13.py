4c16d1695567eb91abfab9809f3f2db128c82261
Kaspooky <kd352@Njit.edu>
2022-05-19 19:28:43 -0400

    Create LeetCode-13.py
    Added a solution to LeetCode problem 13

---
+++ LeetCode-13.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        vals = {}
+        answer = []
+        vals[nums[0]] = 0
+        for i in range(1,len(nums)):
+            diff = target - nums[i]
+            if diff in vals:
+                answer.append(i)
+                answer.append(vals[diff])
+                return answer
+            else:
+                vals[nums[i]] = i
+            
+            
