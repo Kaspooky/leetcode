135c702da8059cd20ee574e1d904e0b9d6c6318c
Kaspooky <kd352@Njit.edu>
2022-06-17 16:05:23 -0400

    Update LeetCode-13.py
    Corrected to the correct problem.

--- LeetCode-13.py
+++ LeetCode-13.py
@@ -1,15 +1,20 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        vals = {}
-        answer = []
-        vals[nums[0]] = 0
-        for i in range(1,len(nums)):
-            diff = target - nums[i]
-            if diff in vals:
-                answer.append(i)
-                answer.append(vals[diff])
-                return answer
+    def romanToInt(self, s: str) -> int:
+        dict = {}
+        
+        dict["I"] = 1
+        dict["V"] = 5
+        dict["X"] = 10
+        dict["L"] = 50
+        dict["C"] = 100
+        dict["D"] = 500
+        dict["M"] = 1000
+        value = dict[s[0]]
+        for index in range(1,len(s)):
+            if dict[s[index]] > dict[s[index-1]]:
+                value-= 2*dict[s[index-1]]
+                value+= dict[s[index]]
             else:
-                vals[nums[i]] = i
-            
-            
+                value+=dict[s[index]]
+        return value
+        
